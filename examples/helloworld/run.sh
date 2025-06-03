8e4d08deefa6a62323def91add984d7f927c0cee
jonathancasters <jonathancasters284@gmail.com>
2025-06-03 09:04:37 +0200

    enable different logging levels through CLI parameter

--- examples/helloworld/run.sh
+++ examples/helloworld/run.sh
@@ -1,3 +1,3 @@
 echo "<-- Executing the helloworld.yaml configuration with the wasm operator framework: -->"
-cargo run --manifest-path ../../parent/Cargo.toml ./helloworld.yaml
+cargo run --manifest-path ../../parent/Cargo.toml ./helloworld.yaml --debug
 echo "<-- Finished executing the example -->"
